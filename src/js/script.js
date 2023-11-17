4e67c2fe556101bc6858ef61b45c371da2b4554e
Stacy <anastasiass96@gmail.com>
2023-11-17 18:02:12 +0100

    First commit

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   var image = document.getElementsByClassName("parallax-img");
+//   new simpleParallax(
+//     image
+//        {
+//       // Опции параллакса (необязательно)
+//       scale: 1.2,
+//       orientation: "up",
+//       delay: 0.6,
+//       transition: "cubic-bezier(0,0,0,1)",
+//       // Другие настройки...
+//     }
+//   );
+// });
